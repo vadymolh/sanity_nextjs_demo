21e7fd7ee35513fd414adcc2d705e4026760a844
vadymvv <vadymolhovyk@gmail.com>
2022-06-08 12:18:15 +0300

    Links

--- blog-frontend/Components/PostListTest.js
+++ blog-frontend/Components/PostListTest.js
@@ -1,4 +1,5 @@
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 const PostListTest = ({ posts, handleDelete}) => {
     console.log(posts);
@@ -6,7 +7,11 @@ const PostListTest = ({ posts, handleDelete}) => {
         <div className={styles.container}>
         {posts.map((post) =>(
             <div className={styles.card} key={post.id}>
-              <h2>{ post.title }</h2>
+              <Link href={"/post/" + post.id}  key={post.id}>
+                <a>
+                  <h2>{ post.title }</h2>
+                </a>
+              </Link>
               <p>{ post.content }</p>
               <button onClick={()=> handleDelete(post.id)}>Delete Post</button>
             </div>
